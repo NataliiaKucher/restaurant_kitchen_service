1730f1ac707706e804e4f8daf504c18333838e29
Nataliia <natali4.kucher55@gmail.com>
2024-03-10 18:29:33 +0100

    Committing from dish_catalog admin and forms files, from restaurant_kitchen_service settings and urls files without changes

--- dish_catalog/admin.py
+++ dish_catalog/admin.py
@@ -1,3 +1,36 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
 
-# Register your models here.
+from dish_catalog.models import Dish, DishType, Cook
+
+
+@admin.register(Dish)
+class DishAdmin(admin.ModelAdmin):
+    list_display = ["name", "price", "description",]
+    list_filter = ["price",]
+    search_fields = ["name"]
+
+
+@admin.register(Cook)
+class CookAdmin(admin.ModelAdmin):
+    list_display = UserAdmin.list_display + ("years_of_experience",)
+    fieldsets = UserAdmin.fieldsets + (
+        (("Additional info", {"fields": ("years_of_experience",)}),)
+    )
+    add_fieldsets = UserAdmin.add_fieldsets + (
+        (
+            (
+                "Additional info",
+                {
+                    "fields": (
+                        "first_name",
+                        "last_name",
+                        "years_of_experience",
+                    )
+                },
+            ),
+        )
+    )
+
+
+admin.site.register(DishType)
